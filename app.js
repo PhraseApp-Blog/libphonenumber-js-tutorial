ab4ec13f1b2a8a361daf8063abe1d1b5e3f9731f
Preetish HS <preetishhs24@gmail.com>
2021-09-08 16:27:39 +0530

    update UI

--- app.js
+++ app.js
@@ -1,89 +1,108 @@
-let examples
+let examples = [], country = '', country2 = '', useCountryCode = false
+let inputText = 'For tech support call +7 (800) 555-35-35 internationally or reach a local US branch at (213) 373-4253 ext. 1234.'
 let xmlhttp = new XMLHttpRequest();
 let url = "https://unpkg.com/libphonenumber-js@1.9.6/examples.mobile.json";
 
-xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    examples = JSON.parse(this.responseText);
-    console.log('Example phone no. of US', libphonenumber.getExampleNumber('US', examples))
-  }
-};
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
+fetch(url)
+  .then(response => response.json())
+  .then(response => {
+    example = response
+  })
 
 
 let mData = new libphonenumber.Metadata()
 let countryListHTML
 function fetchCountries() {
- let countries = libphonenumber.getCountries()
- for( let i = 0; i < countries.length; i++) {
-  countryListHTML+= `<option value="${countries[i]}" >${countries[i]}</option>` 
- }
+  let countries = libphonenumber.getCountries()
+  console.log(countries)
+  for (let i = 0; i < countries.length; i++) {
+    countryListHTML += `<option value="${countries[i]}" >${countries[i]}</option>`
+  }
 }
 fetchCountries()
 
-var country 
 setTimeout(() => {
- document.getElementById("countrySelect").innerHTML = countryListHTML
- document.getElementById("countrySelect2").innerHTML = countryListHTML
-country = document.getElementById("countrySelect").value;
+  document.getElementById("countrySelect").innerHTML = countryListHTML
+  document.getElementById("countrySelect2").innerHTML = countryListHTML
+  document.getElementById("textbox").value = inputText
+  country = document.getElementById("countrySelect").value;
+  country2 = document.getElementById("countrySelect2").value;
+  let val = libphonenumber.findPhoneNumbersInText(inputText)
+  displayValue(val)
 }, 1000)
+
+function parseNo(e) {
+  try {
+
+    let numberObjEvt = libphonenumber.parsePhoneNumber(e.value, country)
+    document.querySelector('.output1').innerHTML = `
+  <span class="font-weight-bold">National Format:</span> ${numberObjEvt.format('NATIONAL')}
+  <br><span class="font-weight-bold">International Format:</span> ${numberObjEvt.format('INTERNATIONAL')}
+  <br><span class="font-weight-bold">RFC3966:</span>  ${numberObjEvt.format('RFC3966')}
+  <br><span class="font-weight-bold">IDD:</span>  ${numberObjEvt.format('IDD', { fromCountry: 'US' })}
+  <br><span class="font-weight-bold">National without Prefix:</span>  ${numberObjEvt.format('NATIONAL', { nationalPrefix: false })}
+  <br><span class="font-weight-bold">Type:</span>  ${numberObjEvt.getType()}
+  <br><span class="font-weight-bold">URI:</span>  ${numberObjEvt.getURI()}
+  <br><span class="font-weight-bold">Non Geographic (Global):</span>  ${numberObjEvt.isNonGeographic()}
+  <br><span class="font-weight-bold">Possible:</span>  ${numberObjEvt.isPossible()}
+  <br><span class="font-weight-bold">Valid:</span>  ${numberObjEvt.isValid()}
+  <br><span class="font-weight-bold">Parse Incomplete Number:</span>  ${libphonenumber.parseIncompletePhoneNumber(e.value)}
+  <br><span class="font-weight-bold">Format Incomplete Number (without Country code):</span>  ${libphonenumber.formatIncompletePhoneNumber(e.value)}
+  <br><span class="font-weight-bold">Format Incomplete Number (with country code):</span>  ${libphonenumber.formatIncompletePhoneNumber(e.value, country)}
+  <br><span class="font-weight-bold">Is country code (${country}) Supported:</span>  ${libphonenumber.isSupportedCountry(country)}
+  `
+  }
+  catch (err) {
+    console.log(err)
+    if (e.value) {
+      document.querySelector('.output1').innerHTML = `<h5> ${err.message} </h5>`
+    } else {
+      document.querySelector('.output1').innerHTML = ''
+    }
+  }
+}
+
 function changeCountry(e) {
   country = e.value
   parseNo(document.getElementById('phone'))
 }
-var numberObjEvt
-function parseNo(e) {
-  numberObjEvt = libphonenumber.parsePhoneNumber(e.value, country)
-
-  document.querySelector('.output1').innerText = `
-  National Format: ${numberObjEvt.format('NATIONAL')}
-  International Format: ${numberObjEvt.format('INTERNATIONAL')}
-  RFC3966: ${numberObjEvt.format('RFC3966')}
-  IDD: ${numberObjEvt.format('IDD', {fromCountry: 'US'})}
-  National without Prefix: ${numberObjEvt.format('NATIONAL', {nationalPrefix: false})}
-  Type: ${numberObjEvt.getType()}
-  URI: ${numberObjEvt.getURI()}
-  Non Geographic: ${numberObjEvt.isNonGeographic()}
-  Possible: ${numberObjEvt.isPossible()}
-  Valid: ${numberObjEvt.isValid()}
-  Parse Incomplete Number: ${libphonenumber.parseIncompletePhoneNumber(e.value)}
-  Format Incomplete Number (without Country code): ${libphonenumber.formatIncompletePhoneNumber(e.value)}
-  Format Incomplete Number (with country code): ${libphonenumber.formatIncompletePhoneNumber(e.value, country)}
-  Is ${country} Supported: ${libphonenumber.isSupportedCountry(country)}
-  ext prefix of ${country}: ${libphonenumber.getExtPrefix(country)}
-  ext prefix of ${country}: ${libphonenumber.getExtPrefix(country)}
-  `
-}
-
-var country2
-setTimeout(() => {
-  country2 = document.getElementById("countrySelect2").value;
-  document.getElementById('textbox').value = 'For tech support call +7 (800) 555-35-35 internationally or reach a local US branch at (213) 373-4253 ext. 1234'
-  let val = libphonenumber.findPhoneNumbersInText(document.getElementById('textbox').value, country2)
-  displayValue(val)
-}, 1000)
 
 function changeCountry2(e) {
   country2 = e.value
-  let val = libphonenumber.findPhoneNumbersInText(document.getElementById('textbox').value, country2)
-  displayValue(val)
+  fetchPhoneNumberInText()
 }
 
 function findPhoneNumber(e) {
-  let val = libphonenumber.findPhoneNumbersInText(e.value, country2)
+  inputText = e.value
+  fetchPhoneNumberInText()
+}
+
+function fetchPhoneNumberInText() {
+  let val
+  if (useCountryCode) {
+    val = libphonenumber.findPhoneNumbersInText(inputText, country2)
+  } else {
+    val = libphonenumber.findPhoneNumbersInText(inputText)
+  }
   displayValue(val)
 }
 
-function displayValue(value) {
+function updateCountryRequired(e) {
+  useCountryCode = e.checked
+  fetchPhoneNumberInText()
+}
+
+function displayValue(foundList) {
+  console.log(foundList)
   let text = ''
-  for(let i = 0; i < value.length; i++) {
+  foundList.forEach(item => {
     text += `
-    country: ${value[i].number.country}
-    countryCallingCode: ${value[i].number.countryCallingCode}
-    nationalNumber: ${value[i].number.nationalNumber}
-    number: ${value[i].number.number}
-    `
-  }
-  document.querySelector('.output2').innerText = text
+    <span class="font-weight-bold">country: </span>${item.number.country}
+     <br><span class="font-weight-bold">countryCallingCode: </span> ${item.number.countryCallingCode}
+     <br><span class="font-weight-bold">nationalNumber: </span>${item.number.nationalNumber}
+     <br><span class="font-weight-bold">number: </span>${item.number.number}
+     ${item.number.ext ? `<br><span class="font-weight-bold">Extension: </span>${item.number.ext}` : ''}<br> <br>
+     `
+  })
+  document.querySelector('.output2').innerHTML = text
 }
